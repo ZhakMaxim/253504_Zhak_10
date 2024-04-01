4a54f563ba13253d7764c9ccef0f7c0fc6157d66
ZhakMaxim <148795239+ZhakMaxim@users.noreply.github.com>
2024-04-01 09:50:19 +0300

    Delete IGI/LR3/IGILab3 directory

--- IGI/LR3/IGILab3/decorators.py
+++
@@ -1,6 +0,0 @@
-def taskName(func):
-    def wrapper(*args, **kwargs):
-        print(f"running function : {func.__name__}")
-        result = func(*args, **kwargs)
-        return result
-    return wrapper
