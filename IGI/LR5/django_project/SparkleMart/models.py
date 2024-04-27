85c3b79bce6e596223483b89d0076b2fbe6a8f11
Maxim Zhak <MaksimZhak2005@gmail.com>
2024-04-27 18:21:54 +0300

    added purchases and promos

--- IGI/LR5/django_project/SparkleMart/models.py
+++ IGI/LR5/django_project/SparkleMart/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import AbstractUser
 import re
 from django.core.exceptions import ValidationError
 
+
 class User(AbstractUser):
     STATUS_CHOICES = (
         ("employee", "employee"),
@@ -19,6 +20,7 @@ class User(AbstractUser):
         if not re.fullmatch(phone_number_pattern, str(self.phone_number)):
             raise ValidationError("This field accepts mail id of google only")
         super().save(*args, **kwargs)
+
 
 class Producer(models.Model):
     name = models.CharField(max_length=255)
@@ -52,3 +54,32 @@ class Order(models.Model):
     amount = models.PositiveSmallIntegerField(default=1)
     price = models.FloatField()
     date = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+    promo_code = models.CharField(max_length=8, null=True)
+
+    def apply_promo(self, promo):
+        if PromoUsage.objects.filter(user_id=self.user, promo_id=promo).exists():
+            return
+        self.price *= 1 - promo.discount/100
+        self.promo_code = promo.code
+        self.save()
+
+        PromoUsage.objects.create(promo=promo, user=self.user)
+
+
+class Purchase(models.Model):
+    user = models.ForeignKey(User, related_name='purchases', on_delete=models.CASCADE)
+    order = models.OneToOneField(Order, on_delete=models.CASCADE)
+    purchase_date = models.DateTimeField(auto_now_add=True)
+    town = models.CharField(max_length=50)
+    delivery_date = models.DateTimeField()
+
+
+class Promo(models.Model):
+    code = models.CharField(max_length=8)
+    discount = models.PositiveSmallIntegerField()
+
+
+class PromoUsage(models.Model):
+    promo = models.ForeignKey(Promo, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
--- IGI/LR5/django_project/SparkleMart/views.py
+++ IGI/LR5/django_project/SparkleMart/views.py
@@ -1,3 +1,5 @@
+import datetime
+
 from .models import *
 from django.views.decorators.csrf import csrf_exempt
 import json
@@ -9,18 +11,20 @@ from django.contrib.auth import authenticate
 from django.views.generic import *
 from django.core.exceptions import ObjectDoesNotExist
 
+
 class UserRegistrationView(View):
     @csrf_exempt
     def post(self, request):
         body_unicode = request.body.decode('utf-8')
         body = json.loads(body_unicode)
 
-        user = User.objects.create_user(username=body["username"],
-                                        password=body["password"],
-                                        email=body["email"],
-                                        phone_number=body["phone_number"],
-                                        status=body["status"],
-                                        )
+        user = User.objects.create_user(
+                                username=body["username"],
+                                password=body["password"],
+                                email=body["email"],
+                                phone_number=body["phone_number"],
+                                status=body["status"],
+                                )
         if user:
             user_data = {
                 "username": body["username"],
@@ -50,8 +54,9 @@ class UserAuthorizationView(View):
                 "phone_number": body["phone_number"],
                 "status": body["status"],
             }
-
+            return JsonResponse(user_data, safe=False)
         return HttpResponse('login unsuccessfully!')
+
 
 class UserLogoutView(View):
     def get(self, request, *args, **kwargs):
@@ -59,10 +64,12 @@ class UserLogoutView(View):
             auth.logout(request)
         return HttpResponseRedirect('/products')
 
+
 class UserListView(View):
     def get(self, request, *args, **kwargs):
         if request.user.is_authenticated and request.user.status == "employee":
             users = User.objects.filter(status="customer")
+
             users_data = []
             for user in users:
                 users_data.append({
@@ -73,6 +80,7 @@ class UserListView(View):
                 })
             return JsonResponse(users_data, safe=False)
         return HttpResponseNotFound("Page not found")
+
 
 class UserDetailView(View):
     def get(self, request, *args, **kwargs):
@@ -89,6 +97,7 @@ class UserDetailView(View):
                 return JsonResponse(user_data, safe=False)
         return HttpResponseNotFound("Page not found")
 
+
 class ProductListView(ListView):
     model = Product
     queryset = Product.objects.all()
@@ -104,6 +113,7 @@ class ProductListView(ListView):
         products_data = []
         for product in products:
             categories = list(product.category.all().values_list('name', flat=True))
+
             products_data.append({
                 'id': product.id,
                 'name': product.name,
@@ -154,14 +164,31 @@ class ProductDetailView(DetailView):
 
 
 class OrderCreateView(View):
-    def get(self, request, pk, *args, **kwargs):
+    def post(self, request, pk, *args, **kwargs):
         if request.user.is_authenticated and request.user.status == "customer":
             product = Product.objects.get(pk=pk)
+
             body_unicode = request.body.decode('utf-8')
             body = json.loads(body_unicode)
             amount = body["amount"]
+
             order = Order.objects.create(user=request.user, product=product, amount=amount, price=amount*product.price)
-            return HttpResponse('order successfully created!')
+
+            order_data = {
+                "user": order.user.username,
+                "number": order.number,
+                "product_id": order.product.id,
+                "price": order.price,
+                "promo": order.promo_code,
+                "amount": order.amount,
+                "date": order.date,
+                "is_active": order.is_active,
+            }
+            product.amount -= amount
+            product.save()
+            return JsonResponse(order_data, safe=False)
+        elif request.user.is_authenticated and request.user.status == "employee":
+            return HttpResponseNotFound("Page not found")
         else:
             return HttpResponse('please, login for making order!')
 
@@ -171,18 +198,24 @@ class OrderListView(View):
         if request.user.is_authenticated and request.user.status == "employee":
             try:
                 orders = Order.objects.all()
+
                 orders_data = []
                 for order in orders:
                     orders_data.append({
                         "user": order.user.username,
                         "number": order.number,
                         "product_id": order.product.id,
+                        "price": order.price,
+                        "promo": order.promo_code,
+                        "amount": order.amount,
                         "date": order.date,
+                        "is_active": order.is_active,
                     })
                 return JsonResponse(orders_data, safe=False)
             except ObjectDoesNotExist:
                 return HttpResponseNotFound("Page not found")
         return HttpResponseNotFound("Page not found")
+
 
 class OrderDetailView(View):
     def get(self, request, *args, **kwargs):
@@ -190,12 +223,16 @@ class OrderDetailView(View):
             try:
                 pk = self.kwargs.get("pk")
                 order = Order.objects.get(pk=pk)
+
                 order_data = {
                     "user": order.user.username,
                     "number": order.number,
                     "product_id": order.product.id,
+                    "price": order.price,
+                    "promo": order.promo_code,
                     "amount": order.amount,
                     "date": order.date,
+                    "is_active": order.is_active,
                     }
                 return JsonResponse(order_data, safe=False)
             except ObjectDoesNotExist:
@@ -204,13 +241,42 @@ class OrderDetailView(View):
             try:
                 pk = self.kwargs.get("pk")
                 order = Order.objects.get(pk=pk, user_id=request.user.id)
+
                 order_data = {
                     "user": order.user.username,
                     "number": order.number,
                     "product_id": order.product.id,
+                    "price": order.price,
+                    "promo": order.promo_code,
                     "amount": order.amount,
                     "date": order.date,
+                    "is_active": order.is_active,
                 }
+                return JsonResponse(order_data, safe=False)
+            except ObjectDoesNotExist:
+                return HttpResponseNotFound("Page not found")
+
+
+class OrderDeleteView(DeleteView):
+    def post(self, request, *args, **kwargs):
+        if request.user.is_authenticated and request.user.status == "customer":
+            try:
+                pk = self.kwargs.get("pk")
+                order = Order.objects.get(pk=pk, user_id=request.user.id, is_active=True)
+
+                order_data = {
+                    "user": order.user.username,
+                    "number": order.number,
+                    "product_id": order.product.id,
+                    "price": order.price,
+                    "promo": order.promo_code,
+                    "amount": order.amount,
+                    "date": order.date,
+                    "is_active": order.is_active,
+                }
+                order.product.amount += order.amount
+                order.product.save()
+                order.delete()
                 return JsonResponse(order_data, safe=False)
             except ObjectDoesNotExist:
                 return HttpResponseNotFound("Page not found")
@@ -222,6 +288,7 @@ class UserOrderListView(View):
             request.user.is_authenticated and request.user.status == "customer" and\
                 self.kwargs.get("pk") == request.user.id:
             pk = self.kwargs.get("pk")
+
             orders = Order.objects.filter(user_id=pk)
             if orders:
                 orders_data = []
@@ -229,10 +296,128 @@ class UserOrderListView(View):
                     orders_data.append({
                         "number": order.number,
                         "product_id": order.product.id,
+                        "price": order.price,
+                        "promo": order.promo_code,
                         "amount": order.amount,
                         "date": order.date,
+                        "is_active": order.is_active,
                     })
                 return JsonResponse(orders_data, safe=False)
             else:
                 return HttpResponse("There are no orders")
         return HttpResponseNotFound("Page not found")
+
+
+class PurchaseCreateView(CreateView):
+    def post(self, request, *args, **kwargs):
+        if request.user.is_authenticated and request.user.status == "customer":
+            try:
+                pk = self.kwargs.get("pk")
+                order = Order.objects.get(pk=pk, user=request.user, is_active=True)
+
+                body_unicode = request.body.decode('utf-8')
+                body = json.loads(body_unicode)
+
+                delivery_datetime = datetime.datetime.now() + datetime.timedelta(days=1)
+                delivery_datetime = delivery_datetime.replace(hour=17, minute=0, second=0, microsecond=0)
+
+                promo_code = body.get("promo_code")
+                promo = Promo.objects.filter(code=promo_code).first()
+
+                if promo:
+                    order.apply_promo(promo)
+
+                purchase = Purchase.objects.create(
+                    user=request.user,
+                    order=order,
+                    town=body["town"],
+                    delivery_date=delivery_datetime
+                )
+
+                order.is_active = False
+                order.save()
+                purchase_data = {
+                    "order_id": order.number,
+                    "user_id": request.user.id,
+                    "town": body["town"],
+                    "purchase_date": purchase.purchase_date,
+                    "delivery_date": purchase.delivery_date,
+                }
+                return JsonResponse(purchase_data, safe=False)
+            except ObjectDoesNotExist:
+                return HttpResponseNotFound("Page not found")
+        return HttpResponseNotFound("Page not found")
+
+
+class PurchaseListView(View):
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated and request.user.status == "employee":
+            try:
+                purchases = Purchase.objects.all()
+
+                purchases_data = []
+                for purchase in purchases:
+                    purchases_data.append({
+                        "order_id": purchase.order.number,
+                        "user_id": purchase.order.user.id,
+                        "username": purchase.order.user.username,
+                        "town": purchase.town,
+                        "purchase_date": purchase.purchase_date,
+                        "delivery_date": purchase.delivery_date,
+                    })
+                return JsonResponse(purchases_data, safe=False)
+            except ObjectDoesNotExist:
+                return HttpResponseNotFound("Page not found")
+        return HttpResponseNotFound("Page not found")
+
+
+class PurchaseDetailView(View):
+
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated and request.user.status == "employee":
+            try:
+                pk = self.kwargs.get("pk")
+                purchase = Purchase.objects.get(pk=pk)
+
+                purchase_data = {
+                    "order_id": purchase.order.number,
+                    "user_id": purchase.order.user.id,
+                    "username": purchase.order.user.username,
+                    "town": purchase.town,
+                    "purchase_date": purchase.purchase_date,
+                    "delivery_date": purchase.delivery_date,
+                    }
+                return JsonResponse(purchase_data, safe=False)
+            except ObjectDoesNotExist:
+                return HttpResponseNotFound("Page not found")
+        elif request.user.is_authenticated and request.user.status == "customer":
+            try:
+                pk = self.kwargs.get("pk")
+                purchase = Purchase.objects.get(pk=pk, user_id=request.user.id)
+
+                purchase_data = {
+                    "order_id": purchase.order.number,
+                    "user_id": purchase.order.user.id,
+                    "username": purchase.order.user.username,
+                    "town": purchase.town,
+                    "purchase_date": purchase.purchase_date,
+                    "delivery_date": purchase.delivery_date,
+                }
+                return JsonResponse(purchase_data, safe=False)
+            except ObjectDoesNotExist:
+                return HttpResponseNotFound("Page not found")
+
+
+class PromoListView(View):
+    def get(self, request, *args, **kwargs):
+        promos = Promo.objects.all()
+        promos_data = []
+
+        for promo in promos:
+            promos_data.append({
+                "code": promo.code,
+                "discount": promo.discount,
+            })
+        return JsonResponse(promos_data, safe=False)
+
+
